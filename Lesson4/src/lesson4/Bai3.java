e64bacc8504dae86fa449ef4484fa0476c004d4a
mylv170998 <52785713+mylv170998@users.noreply.github.com>
2019-09-15 14:55:14 +0700

    bai3

---
+++ Lesson4/src/lesson4/Bai3.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lesson4;
+
+/**
+ *
+ * @author mylv
+ */
+public class Bai3 {
+    public static void main(String[] args){
+        char[] a={'m','y','d','e','p','t','r','a','i'};
+        char[] b=sapxep (a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i] + "  ");
+            
+        }
+    }
+      static char[] sapxep(char[] a){
+          char n = 0;
+          for (int i=0;i<a.length;i++){
+              for(int j=0;j<a.length-1;j++){
+                  if (a[i]<a[j]) {
+                      n=a[i];
+                      a[i]=a[j];
+                      a[j]=n;
+                  }
+                  
+              }
+          }
+          return a;
+      }  
+    
+    
+}
